1b5b5a65d40f5b4f06e8d218c85798a977447505
XinLoiDuongQue <100332815+XinLoiDuongQue@users.noreply.github.com>
2025-06-26 02:30:22 +0000

    Updated Paddle movements

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,6 +10,8 @@ export class Game extends Scene {
         this.leftPaddle = null;
         this.rightPaddle = null;
         this.ballInMotion = false;
+        this.wasd = null;
+        this.cursor = null;
     }
 
     preload() {
@@ -19,16 +21,30 @@ export class Game extends Scene {
     }
 
     create() {
+    // load images and sprites
         this.add.image(WIDTH/2, HEIGHT/2, 'background').setScale(0.8, 0.8);
         this.leftPaddle = this.add.image(50, 384, "paddle");
         this.rightPaddle = this.add.image(974, 384, "paddle");
+    // load ball and ball physics
         this.ball = this.physics.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0.05, 0.05).refreshBody();
         this.ball.setCollideWorldBounds(true);
         this.ball.setBounce(1,1);
         this.input.keyboard.on("keydown-SPACE", this.startBall, this);
+    // set up paddle physics
+        this.cursor = this.input.keyboard.createCursorKeys();
+        this.wasd = this.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+        });
     }
 
     update() {
+        //make the paddles move
+        if (this.cursor.down.isDown && this.rightPaddle.y < HEIGHT - this.rightPaddle.height /2) {this.rightPaddle.y += 5;}
+        else if (this.cursor.up.isDown && this.rightPaddle.y > this.rightPaddle.height /2) {this.rightPaddle.y -= 5;}
+
+        if (this.wasd.up.isDown && this.leftPaddle.y > this.leftPaddle.height /2) {this.leftPaddle.y -= 5;}
+        else if (this.wasd.down.isDown && this.leftPaddle.y < HEIGHT - this.leftPaddle.height /2) {this.leftPaddle.y += 5;}
     }
 
     startBall(){
