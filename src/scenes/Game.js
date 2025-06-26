10f9088bfb7b0e46f6af10a543587088023960a9
XinLoiDuongQue <100332815+XinLoiDuongQue@users.noreply.github.com>
2025-06-26 01:25:25 +0000

    Created sprite for the game

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,12 +6,22 @@ const HEIGHT = 768;
 export class Game extends Scene {
     constructor() {
         super('Game');
+        this.ball = null;
+        this.LeftPaddle = null;
+        this.RightPaddle = null;
     }
 
     preload() {
+        this.load.image("background", "assets/background.png");
+        this.load.image("ball", "assets/ball.png");
+        this.load.paddle("paddle", "assets/paddle.png")
     }
 
     create() {
+        this.add.image(WIDTH/2, HEIGHT/2, 'background').setScale(0.8, 0.8);
+        this.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0,5, 0.5);
+        this.LeftPaddle = this.add.image(50, 384, 'paddle');
+        this.RightPaddle = this.add.image(950, 384, 'paddle');
     }
 
     update() {
