cb008fa3981f9209b21f4b2eaf50829fdfb877a4
XinLoiDuongQue <100332815+XinLoiDuongQue@users.noreply.github.com>
2025-06-26 02:02:08 +0000

    Updated Ball Physics

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,14 +6,13 @@ const HEIGHT = 768;
 export class Game extends Scene {
     constructor() {
         super('Game');
-        // Initialise necessary variables
         this.ball = null;
         this.leftPaddle = null;
         this.rightPaddle = null;
+        this.ballInMotion = false;
     }
 
     preload() {
-        // Load necessary assets from the assets directory
         this.load.image('background', 'assets/background.png');
         this.load.image('ball', 'assets/ball.png');
         this.load.image('paddle', 'assets/paddle.png');
@@ -24,9 +23,19 @@ export class Game extends Scene {
         this.ball = this.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0.05, 0.05);
         this.leftPaddle = this.add.image(50, 384, "paddle");
         this.rightPaddle = this.add.image(974, 384, "paddle");
+        this.ball = this.physics.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0.05, 0.05).refreshBody();
+        this.ball.setCollideWorldBounds(true);
+        this.ball.setBounce(1,1);
+        this.input.keyboard.on("keydown-SPACE", this.startBall, this);
     }
 
     update() {
     }
 
+    startBall(){
+    if (!this.ballInMotion) {
+        this.ball.setVelocity(200, 200);
+        this.ballInMotion = true;
+    }
+    }
 }
